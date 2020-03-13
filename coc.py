7937ba7fdf8a331fb9e89a1066a8c6c650536c77
Joseph Bozeman <joseph.l.bozeman@gmail.com>
2020-03-12 20:02:19 -0500

    Changed some infra for extensibility & cleanliness

--- coc.py
+++ coc.py
@@ -1,6 +1,8 @@
 from dice import dice_pool, dice_roll, d6_pool, Die
+from abc import ABCMeta, abstractmethod
 from functools import reduce
 from math import ceil
+from random import sample
 import csv
 
 # The damage bonus table can be found on page 33 of the 7th revised edition.
@@ -25,32 +27,31 @@ def damage_bonus_table(strength, size):
         damage_bonus_string = str(damage_bonus)
         build = damage_bonus
     return {'damage-bonus': damage_bonus, 'build': build, 'dice': damage_bonus_string}
+# * being able to read in from a skill csv could be extended to read from a characteristics + skill csv
 
-class Skill(object):
-    def __init__(self, name, score, check=False, rules=None, important=False):
-        self.name = name
+class Addable(object,metaclass=ABCMeta):
+    def __init__(self, score):
         self.score = score
+
+    def __add__(self, x):
+        return self.score + x
+
+    def __radd__(self, x):
+        return self.score + x
+
+
+class Rollable(Addable,metaclass=ABCMeta):
+    # TODO: make it so that all the reading methods are inside here as static or class methods
+    def __init__(self, name, score, check):
+        self.name = name
+        super().__init__(score)
         self.check = check
-        self.rules = rules
-        self.important = important
 
-    def points(self):
-        return self.score
-
-    def check(self, roll):
-        if check == True:
-            if roll > score:
-                self.score += Die(6).result
-
-    def roll(self):
-        result = Die(100).result
-        outcome = True if (result <= self.score) else False
-        if outcome is True:
-            self.check = True
-        return {"outcome": outcome, "result": result}
-
-    def add_points(self, points):
-        self.score += points
+    # Remember to fix this and add error handling later
+    @classmethod
+    def from_tuple(cls, name_score_tuple):
+        rollable = cls(name=name_score_tuple[0], score=name_score_tuple[1])
+        return rollable
 
     def __repr__(self):
         box = "\u25a1"
@@ -63,9 +64,47 @@ class Skill(object):
         checked = "\u2611"
         msg = f"{box if self.check is False else checked} {self.name.title()}: {self.score}"
         return msg
+    
+    def points(self):
+        return self.score
+
+    def check(self, roll, check=self.check):
+        if check == True:
+            if roll > self.score:
+                self.score += Die(6).result
+
+    def roll(self):
+        result = Die(100).result
+        outcome = True if (result <= self.score) else False
+        if outcome is True:
+            self.check = True
+        return {"outcome": outcome, "result": result}
+
+    def add_points(self, points):
+        self.score += points
+
+class Skill(Rollable):
+    def __init__(self, name, score, check=False, rules=None, important=False):
+        super().__init__(name, score, check)
+        self.rules = rules
+        self.important = important 
+
+    @classmethod
+    def from_tuple(cls, name_score_tuple):
+        skill = super().from_tuple(name_score_tuple)
+        return skill
+
+class Characteristic(Rollable):
+    def __init__(self, name, short_name=name[0:3], score=0, check=False, rules=None):
+        super().__init__(name, score, check)
+        self.short_name = short_name.upper()
+    
+    def __repr__(self):
+        msg = f"{self.short_name}: {self.score}"
+        return msg
 
 
-class HumanCharacter(object):
+class Investigator(object):
     
     @staticmethod
     def read_skill_dict(filename="skills.csv"):
@@ -84,7 +123,7 @@ class HumanCharacter(object):
             "CON": self.constitution,
             "POW": self.power,
             "DEX": self.dexterity,
-            "CHA": self.charisma,
+            "APP": self.appearance,
             "INT": self.intelligence,
             "SIZ": self.size,
             "EDU": self.education,
@@ -97,63 +136,67 @@ class HumanCharacter(object):
         ret = self.skill_dict[skill_name].points()
         return ret
 
+    def apply_age_penalty(self, strength, constitution, size, dexterity, appearance, intelligence, power, education, luck):
+        if self.age >= 80:
+            self.strength = strength
+
+    # TODO: implement the age penalty. more complicated than I thought
+    # TODO: make a new constructor for Skills and Characteristics to pull from a tuple like (Skill_name, score).
+    # XXX: Characteristic could derive the short_name from the first 3 letters of the name
+    # XXX: that will make it easier to implement rule packs and the csvs
+
     @classmethod
     def generate_random(cls, name,skill_dict):
-        # TODO: update this to 7th edition rules!!!
-        # STR, CON, POW, DEX, CHA are all 3d6
-        strength = dice_roll(d6_pool(3))
-        constitution = dice_roll(d6_pool(3))
-        power = dice_roll(d6_pool(3))
-        dexterity = dice_roll(d6_pool(3))
-        charisma = dice_roll(d6_pool(3))
+        # RULES: STR, CON, POW, DEX, APP are all 3d6
+        # RULES: INT and SIZ are 2d6 + 6
+        # RULES: EDU is 2d6+6
+        # RULES: under 7th edition rules I multiply them by 5
+        # RULES: under the laundry files age is 17 + 2d6, but we are hard coding 7e for now
 
-        # INT and SIZ are 2d6 + 6
-        intelligence = dice_roll(d6_pool(2), 6)
-        size = dice_roll(d6_pool(2), 6)
-
-        # EDU is 3d6+3
-        education = dice_roll(d6_pool(3), 3)
-        # Age is 17+2d6, but has a minimum of EDU + 5
-        age = dice_roll(d6_pool(2), 17)
-        age = age if age >= (education + 5) else (education + 5)
-
+        age = sample(range(15,90),1)       
         character = cls(
-            name,
-            strength,
-            constitution,
-            power,
-            dexterity,
-            charisma,
-            intelligence,
-            size,
-            education,
-            age,
-            skill_dict,
+            name=name,
+            strength=Characteristic("Strength", "STR", dice_roll(d6_pool(3)) * 5),
+            constitution=Characteristic("Constitution", "CON", dice_roll(d6_pool(3)) * 5),
+            size=Characteristic("Size", "SIZ", dice_roll(d6_pool(2),6) * 5),
+            dexterity=Characteristic("Dexterity", "DEX", dice_roll(d6_pool(3)) * 5),
+            appearance=Characteristic("Appearance", "APP", dice_roll(d6_pool(3)) * 5),
+            intelligence=Characteristic("Intelligence", "INT", dice_roll(d6_pool(2),6) * 5),
+            power=Characteristic("Power", "POW", dice_roll(d6_pool(3)) * 5),
+            education=Characteristic("Education", "EDU", dice_roll(d6_pool(2),6) * 5),
+            luck=Characteristic("Luck", "LUK", dice_roll(d6_pool(3)) * 5),
+            age=age,
+            skill_dict=skill_dict,
         )
+        character.apply_age_penalty(age)
+
+
         return character
 
 
     def __init__(
         self,
         name,
-        strength,
-        constitution,
-        power,
-        dexterity,
-        charisma,
-        intelligence,
-        size,
-        education,
+        strength: Characteristic,
+        constitution: Characteristic,
+        power: Characteristic,
+        dexterity: Characteristic,
+        appearance: Characteristic,
+        intelligence: Characteristic,
+        size: Characteristic,
+        education: Characteristic,
+        luck: Characteristic,
         age,
         skill_dict,
     ):
         self.name = name
-        # STR, CON, POW, DEX, CHA are all 3d6
+        # STR, CON, POW, DEX, APP are all 3d6
         self.strength = strength
         self.constitution = constitution
         self.power = power
         self.dexterity = dexterity
-        self.charisma = charisma
+        self.appearance = appearance
+        self.luck = luck
 
         # INT and SIZ are 2d6 + 6
         self.intelligence = intelligence
@@ -183,7 +226,7 @@ class HumanCharacter(object):
             f"Name: {self.name}\n Age: {self.age:>2}\n"
             f" STR: {self.strength:>2} | INT: {self.intelligence:>2}\n"
             f" CON: {self.constitution:>2} | POW: {self.power:>2}\n"
-            f" DEX: {self.dexterity:>2} | CHA: {self.charisma:>2}\n"
+            f" DEX: {self.dexterity:>2} | APP: {self.appearance:>2}\n"
             f" SIZ: {self.size:>2} | EDU: {self.education:>2}\n"
             f"  HP: {self.current_hp}/{self.max_hp}"
         )
@@ -194,29 +237,14 @@ class HumanCharacter(object):
     def skill_roll(self, skill_name):
         skill = self.skill_dict[skill_name]
         return skill.roll()
+# TODO: refactor this to be a subclass of Skill, to use the improvement check method.
 
-class Characteristic(object):
-    def __init__(self, name, short_name, score):
-        self.name = name
-        self.short_name = short_name
-        self.score = score
-
-    def __repr__(self):
-        msg = f"{self.short_name}: {self.score}"
-        return msg
-
-    def __add__(self, x):
-        return self.score + x
-
-    def __radd__(self, x):
-        return self.score + x
 
 if __name__ == "__main__":
     print("Welcome to the Call of Cthulhu character generator.")
     name = input("Please provide a name for your character: ")
-    skill_dict = HumanCharacter.read_skill_dict()
+    skill_dict = Investigator.read_skill_dict()
     # Now we will start creating a character.
-    # TODO: Generate characteristics
     stat_block = {
         "STR" : Characteristic("Strength", "STR", dice_roll(d6_pool(3)) * 5),
         "CON" : Characteristic("Constitution", "CON", dice_roll(d6_pool(3)) * 5),
